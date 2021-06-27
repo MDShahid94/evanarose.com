0071d637af909e921dd32a7fcf136a1b52c6d756
MDShahid94 <thewitnessone@gmail.com>
2021-06-27 16:18:49 +0530

    Update tooltips.js

--- js/tooltips.js
+++ js/tooltips.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
 
     $('[data-toggle="popover"]').popover({
         placement : 'right',
-        trigger: 'click',
+        trigger: 'click hover',
         html: true
     });
 
