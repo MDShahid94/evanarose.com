f1b45597078fb3a66031546eb65d01458e182c49
MDShahid94 <thewitnessone@gmail.com>
2021-06-27 14:12:21 +0530

    Update tooltips.js

--- js/tooltips.js
+++ js/tooltips.js
@@ -11,30 +11,18 @@ $(document).ready(function(){
 /* Set the location where mydoc_tooltips_source.json is. */
 var url = "tooltips.json";
 
-$.get( url, function( data ) {
+    $.get( url, function( data ) {
 
- /* Bootstrap popover text is defined inside a data-content attribute inside an element. That's
- why I'm using attr here. If you just want to insert content on the page, use append and remove the data-content argument from the parentheses.*/
+    /* Bootstrap popover text is defined inside a data-content attribute inside an element. That's
+    why I'm using attr here. If you just want to insert content on the page, use append and remove the data-content argument from the parentheses.*/
 
-    $.each(data.entries, function(i, page) {
-        if (page.doc_id == document.querySelector('.tooltips').id) {
-            $( "#basketball" ).attr( "data-content", page.body );
-        }
-
-        if (page.doc_id == "baseball") {
-            $( "#baseball" ).attr( "data-content", page.body );
-        }
-        if (page.doc_id == "football") {
-            $( "#football" ).attr( "data-content", page.body );
-        }
-
-        if (page.doc_id == "soccer") {
-            $( "#soccer" ).attr( "data-content", page.body );
-        }
-
-
+        $.each(data.entries, function(i, page) {
+            $(document).on('click', '.popover-content' , function(){
+                var id = $(this).attr('data-id');
+                if (page.doc_id == id) {
+                    $( '#' + id ).attr( "data-content", page.body );
+                }
+            });
         });
     });
-
-
 });
